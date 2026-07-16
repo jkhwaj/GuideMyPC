3840f78cc9105204b5c93457a4fb9f421bb3fb63
jkhwaj <jkhwaj@gmail.com>
2026-07-16 19:52:28 +0300

    updated SQL

--- database/guidemypc.sql
+++ database/guidemypc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2026 at 01:09 AM
+-- Generation Time: Jul 16, 2026 at 06:51 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
